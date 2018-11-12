4364f54640cbc6f31101cd2a9eb01c9b6c74d4ec
petrelevich <petrelevich@yandex.ru>
2018-11-13 02:24:26 +0300

    L02.1 memory, class Loader

---
+++ L02.1-memory/src/main/java/ru/otus/l21/classLoader/ClassForLoading.java
@@ -0,0 +1,14 @@
+package ru.otus.l21.classLoader;
+
+/**
+ * @author sergey
+ * created on 12.11.18.
+ */
+public class ClassForLoading {
+    public ClassForLoading() {
+    }
+
+    public void action() {
+        System.out.println("test action");
+    }
+}
