367f479a7e3c9b2d3e9ae5382ff0ef78103fda3d
sergey <petrelevich@yandex.ru>
2019-01-29 01:47:30 +0300

    L8.2 creational patterns

--- L09.1-io/src/main/java/ru/otus/l091/io/Person.java
+++ L09.1-io/src/main/java/ru/otus/l091/io/Person.java
@@ -6,17 +6,21 @@ import java.io.Serializable;
  * @author sergey
  * created on 27.01.19.
  */
+//implements Serializable - обязательное условие для сериализации
 public class Person implements Serializable {
     private final int age;
     private final String name;
+    private transient final String hidden = "hiddenField"; //transient - поле будет пропущено при сериализации
 
+    // Обратите внимание на то, сколько раз вызывается констурктор и сколько объектов создается
     Person(int age, String name) {
+        System.out.println("new Person");
         this.age = age;
         this.name = name;
     }
 
     @Override
     public String toString() {
-        return "Name: " + this.name + " age: " + this.age;
+        return "Name: " + name + " age: " + age + " hidden:" + hidden;
     }
 }
